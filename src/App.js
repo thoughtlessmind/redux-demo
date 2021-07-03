c873800fe74e1b4b0f1520e92041f33918b8e979
Rajiv <1998irajeev@gmail.com>
2021-07-03 12:54:47 +0530

    react tempalte removed

--- src/App.js
+++ src/App.js
@@ -9,20 +9,7 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>Working</p>
       </div>
     </Provider>
   )
