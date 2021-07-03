bbdaae9d4b18e5bbdc590ab4b5fd0795dc95a713
Rajiv <1998irajeev@gmail.com>
2021-07-03 12:52:48 +0530

    redux and redux-thunk added

---
+++ src/store.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+
+import { combineReducers } from 'redux'
+
+
+const rootReducer = combineReducers({
+  
+})
+
+const configureStore = () => {
+  let middlewares = [thunk]
+  const store = createStore(
+    rootReducer,
+    process.env.NODE_ENV === 'development'
+      ? window.__REDUX_DEVTOOLS_EXTENSION__
+        ? compose(
+            applyMiddleware(...middlewares),
+            window.__REDUX_DEVTOOLS_EXTENSION__({
+              trace: true,
+              traceLimit: 25
+            })
+          )
+        : compose(applyMiddleware(...middlewares))
+      : compose(applyMiddleware(...middlewares))
+  )
+  return store
+}
+
+export default configureStore
